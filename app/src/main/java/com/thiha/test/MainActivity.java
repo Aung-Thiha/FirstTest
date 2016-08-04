f049178b1ce8a083ed283039fedbfa64640cd638
Thiha <aungthiha@cloudsource.co.jp>
2016-08-04 10:00:48 +0630

    initial commit

---
+++ app/src/main/java/com/thiha/test/MainActivity.java
@@ -0,0 +1,33 @@
+package com.thiha.test;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeActionBar();
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.profileedit, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    private void initializeActionBar() {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+}
